89592fef53d230ef093dd2e479e955ed5013346f
Justin <jtsshieh@outlook.com>
2018-11-12 17:30:11 -0500

    First Commit (with existing code)

---
+++ BlemOS/Program.cs
@@ -0,0 +1,25 @@
+﻿using BlemOS.Registry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlemOS
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            PersistentSettings.load();
+            new Main.LoginScreen().Show();
+            Application.Run();
+        }
+    }
+}
